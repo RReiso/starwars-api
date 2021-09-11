60d195475caf7836ac13f37df4bb61375d6bb210
Ruta R <rutareiso@protonmail.com>
2021-09-11 07:19:30 -0400

    Refactor code

--- script.js
+++ script.js
@@ -30,7 +30,9 @@ async function fetchAllFilms() {
 					filmBox.innerHTML = `
           	<h3>Title: <span>${film.title}</span></h3>
             <h4>Episode: <span >${film.episode_id}</span></h4>
-						<h4 class="gold">Release year: <span class="year">${new Date(film.release_date).getFullYear()}</span></h4>`;
+						<h4 class="gold">Release year: <span class="year">${new Date(
+							film.release_date
+						).getFullYear()}</span></h4>`;
 					filmList.appendChild(filmBox);
 					filmBox.addEventListener("click", function () {
 						displayFilmDetails(film);
@@ -88,7 +90,10 @@ function getItemData(itemURLs, list) {
 	list.innerHTML = "";
 
 	if (itemURLs.length === 0) {
-		itemInfo = { name: "This character has not piloted any starships", url: "n/a" };
+		itemInfo = {
+			name: "This character has not piloted any starships",
+			url: "n/a",
+		};
 		displayItem(itemInfo, list);
 	}
 
@@ -120,7 +125,7 @@ function displayItem(itemInfo, list) {
 		item.addEventListener("click", function () {
 			displayStarshipDetails(itemInfo);
 		});
-	} else if (itemInfo.url.includes("films")){
+	} else if (itemInfo.url.includes("films")) {
 		item.addEventListener("click", function () {
 			displayFilmDetails(itemInfo);
 		});
@@ -149,17 +154,21 @@ function displayFilmDetails(filmInfo) {
 
 	const filmDetails = document.querySelector(".film-details");
 	filmDetails.innerHTML = `
-        <p ><span class="gold">Title: </span> ${filmInfo.title} </p>
-        <p><span  class="gold" >Episode: </span>${filmInfo.episode_id} </p>
-        <p ><span class="gold">Release Year: </span>${new Date(filmInfo.release_date).getFullYear()}</p>
-        <p ><span class="gold">Director: </span>${filmInfo.director}</p>
-        <p ><span class="gold">Opening crawl: </span>"${filmInfo.opening_crawl}"</p>`;
+        <p><span class="gold">Title: </span> ${filmInfo.title} </p>
+        <p><span class="gold">Episode: </span>${filmInfo.episode_id} </p>
+        <p><span class="gold">Release Year: </span>${new Date(
+					filmInfo.release_date
+				).getFullYear()}</p>
+        <p><span class="gold">Director: </span>${filmInfo.director}</p>
+        <p><span class="gold">Opening crawl: </span>"${
+					filmInfo.opening_crawl
+				}"</p>`;
 
 	// Fetch data about characters and starships associated with the film:
-	const charactersInFilm = document.querySelector(".character-list");
-	getItemData(filmInfo.characters, charactersInFilm);
-	const starshipsInFilm = document.querySelector(".starship-list");
-	getItemData(filmInfo.starships, starshipsInFilm);
+	const characters = filmInfo.characters;
+	const starships = filmInfo.starships;
+	fetchAssociatedData(".character-list", characters);
+	fetchAssociatedData(".starship-list", starships);
 }
 
 // Display details about a single character:
@@ -168,21 +177,21 @@ function displayCharacterDetails(characterInfo) {
 	characterDiv.classList.add("shadow", "display", "white-bg");
 	const character = document.querySelector(".character-details");
 	character.innerHTML = `
-    <p ><span class="dark-gold">Name: </span> ${characterInfo.name} </p>
-    <p><span  class="dark-gold" >Gender: </span>${characterInfo.gender} </p>
-    <p ><span class="dark-gold">Birth Year: </span>${characterInfo.birth_year} </p>
-    <p ><span class="dark-gold">Height: </span>${characterInfo.height}</p>
-    <p ><span class="dark-gold">Eye Color: </span>"${characterInfo.eye_color}"</p>
-    <p ><span class="dark-gold">Skin Color: </span>"${characterInfo.skin_color}"</p>
-    <p ><span class="dark-gold">Hair Color: </span>"${characterInfo.hair_color}"</p>
-    <p ><span class="dark-gold">Mass: </span>"${characterInfo.mass}"</p>`;
+    <p><span class="dark-gold">Name: </span> ${characterInfo.name} </p>
+    <p><span class="dark-gold">Gender: </span>${characterInfo.gender} </p>
+    <p><span class="dark-gold">Birth Year: </span>${characterInfo.birth_year} </p>
+    <p><span class="dark-gold">Height: </span>${characterInfo.height}</p>
+    <p><span class="dark-gold">Eye Color: </span>"${characterInfo.eye_color}"</p>
+    <p><span class="dark-gold">Skin Color: </span>"${characterInfo.skin_color}"</p>
+    <p><span class="dark-gold">Hair Color: </span>"${characterInfo.hair_color}"</p>
+    <p><span class="dark-gold">Mass: </span>"${characterInfo.mass}"</p>`;
 	document.querySelector(".search-form").scrollIntoView(true);
 
 	// Fetch data about films and starships associated with the character:
-	const filmsWithCharacter = document.querySelector(".character-film-list");
-	getItemData(characterInfo.films, filmsWithCharacter);
-	const starshipsWithCharacter = document.querySelector(".character-starship-list");
-	getItemData(characterInfo.starships, starshipsWithCharacter);
+	const films = characterInfo.films;
+	const starships = characterInfo.starships;
+	fetchAssociatedData(".character-film-list", films);
+	fetchAssociatedData(".character-starship-list", starships);
 }
 
 // Display details about a single starship:
@@ -190,21 +199,27 @@ function displayStarshipDetails(starshipInfo) {
 	const starship = document.querySelector(".starship-details");
 	starship.classList.add("display");
 	starship.innerHTML = `
-  <p ><span class="gold">Name: </span> ${starshipInfo.name} </p>
-  <p><span  class="gold" >Model: </span>${starshipInfo.model} </p>
-  <p ><span class="gold">Manufacturer: </span>${starshipInfo.manufacturer} </p>
-  <p ><span class="gold">Class: </span>${starshipInfo.starship_class}</p>
-  <p ><span class="gold">Length: </span>"${starshipInfo.length}"</p>
-  <p ><span class="gold">Cargo Capacity: </span>${starshipInfo.cargo_capacity}</p>
-  <p ><span class="gold">Hyperdrive Rating: </span>${starshipInfo.hyperdrive_rating}</p>
-  <p ><span class="gold">Max Atmosphering Speed: </span>${starshipInfo.max_atmosphering_speed}</p>
-  <p ><span class="gold">Passengers: </span>${starshipInfo.passengers}</p>
-  <p ><span class="gold">Crew: </span>${starshipInfo.crew}</p>
-  <p ><span class="gold">Consumables: </span>${starshipInfo.consumables}</p>`;
+  <p><span class="gold">Name: </span> ${starshipInfo.name} </p>
+  <p><span class="gold">Model: </span>${starshipInfo.model} </p>
+  <p><span class="gold">Manufacturer: </span>${starshipInfo.manufacturer} </p>
+  <p><span class="gold">Class: </span>${starshipInfo.starship_class}</p>
+  <p><span class="gold">Length: </span>"${starshipInfo.length}"</p>
+  <p><span class="gold">Cargo Capacity: </span>${starshipInfo.cargo_capacity}</p>
+  <p><span class="gold">Hyperdrive Rating: </span>${starshipInfo.hyperdrive_rating}</p>
+  <p><span class="gold">Max Atmosphering Speed: </span>${starshipInfo.max_atmosphering_speed}</p>
+  <p><span class="gold">Passengers: </span>${starshipInfo.passengers}</p>
+  <p><span class="gold">Crew: </span>${starshipInfo.crew}</p>
+  <p><span class="gold">Consumables: </span>${starshipInfo.consumables}</p>`;
 	starship.scrollIntoView();
 }
 
+function fetchAssociatedData(string, itemURLs) {
+	const listElement = document.querySelector(string);
+	getItemData(itemURLs, listElement);
+}
+
 //--- ERROR HANDLING ---//
+
 function handleError(error, DOMelement) {
 	console.error(error);
 	const err = document.createElement("p");
